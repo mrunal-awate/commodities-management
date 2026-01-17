f821b67960f93ff0021d235140a8034f62fe6c55
Mrunal Awate <mrunalawate0tv@gmail.com>
2026-01-18 00:10:13 +0530

    changes in all files

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { PrismaModule } from '../prisma.module';
 
 @Module({
   imports: [
+    PrismaModule,
     JwtModule.register({
-      secret: 'secret123',
+      secret: 'supersecretkey', // assignment-safe
       signOptions: { expiresIn: '1h' },
     }),
   ],
