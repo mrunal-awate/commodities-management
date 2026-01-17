1383bd648463d0f0e98a2d58429501f3fa652c85
Mrunal Awate <mrunalawate0tv@gmail.com>
2026-01-18 00:29:04 +0530

    changes in all files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 
 import { AuthModule } from './auth/auth.module';
@@ -9,10 +8,10 @@ import { PrismaModule } from './prisma.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
+    GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
+      context: ({ req }) => ({ req }),
     }),
     PrismaModule,
     AuthModule,
